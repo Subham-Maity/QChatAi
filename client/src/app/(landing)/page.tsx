ea3b84cab74a0868cbe575fffc4d1aaed1d7617b
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-08-16 10:26:30 +0530

    chore: remove scroll motion

--- client/src/app/(landing)/page.tsx
+++ client/src/app/(landing)/page.tsx
@@ -6,19 +6,16 @@ import Features from "@/components/landing/features/features";
 import Pricing from "@/components/landing/pricing/pricing";
 import Customer from "@/components/landing/customer/customer";
 import Started from "@/components/landing/started/started";
-import { ScrollerMotion } from "scroller-motion";
 
 const HomePage = () => {
   return (
     <section className="landing-theme w-full relative flex items-center justify-center flex-col px-4 md:px-0 py-8">
-      <ScrollerMotion>
-        <Hero />
-        <Work />
-        <Features />
-        <Pricing />
-        <Customer />
-        <Started />
-      </ScrollerMotion>
+      <Hero />
+      <Work />
+      <Features />
+      <Pricing />
+      <Customer />
+      <Started />
     </section>
   );
 };
